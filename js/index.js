0aa144c3fa744af71a642964cd65657664e62c21
LinPin1 <penny7891@yahoo.com.tw>
2021-11-10 18:47:46 +0800

    hamburger

--- js/index.js
+++ js/index.js
@@ -41,9 +41,12 @@ $(function(){
     
     // -open 的樣式切換
     $(this).closest("aside.aside").toggleClass("-open");
+    $(this).closest("button.btn").toggleClass("-on");
     
   });
 
 });
+// 
+// 漢堡
 
  
